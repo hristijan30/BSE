37d05a1c0b48165fd407d68472b999deead67d16
KikoMira <mircevskihristijan30@gmail.com>
2025-10-29 16:58:53 +0100

    added basic thread pooling (limiting how many threads can run at a time)

--- Threading/ThreadingSystem.cpp
+++ Threading/ThreadingSystem.cpp
@@ -28,4 +28,26 @@ namespace BSE
         m_completedTasks.clear();
         return tasks;
     }
+
+    ThreadPool::ThreadPool(ThreadingSystem& threadingSystem, size_t threadCount)
+        : m_threadingSystem(threadingSystem)
+        , m_threadCount(threadCount)
+        , m_arena(static_cast<int>(threadCount == 0 ? std::max(1u, std::thread::hardware_concurrency()) : threadCount))
+    {
+        if (m_threadCount == 0)
+            m_threadCount = std::max<size_t>(1, std::thread::hardware_concurrency());
+    }
+
+    ThreadPool::~ThreadPool()
+    {
+        m_shuttingDown.store(true, std::memory_order_release);
+        WaitAll();
+    }
+
+    void ThreadPool::WaitAll()
+    {
+        m_taskGroup.wait();
+    }
+
 }
+
--- Threading/ThreadingSystem.h
+++ Threading/ThreadingSystem.h
@@ -4,6 +4,7 @@
 #include "../Engine/StandardInclude.h"
 
 #include <tbb/task_group.h>
+#include <tbb/task_arena.h>
 
 namespace BSE
 {
@@ -29,5 +30,58 @@ namespace BSE
 
         std::mutex m_queueMutex;
         std::vector<std::function<void()>> m_completedTasks;
+    };
+
+    class DLL_EXPORT ThreadPool
+    {
+    public:
+        ThreadPool(ThreadingSystem& threadingSystem, size_t threadCount = 0);
+        ~ThreadPool();
+
+        ThreadPool(const ThreadPool&) = delete;
+        ThreadPool& operator=(const ThreadPool&) = delete;
+
+        template<typename Func>
+        void Submit(Func&& task)
+        {
+            if (m_shuttingDown.load(std::memory_order_acquire))
+                return;
+
+            auto work = std::function<void()>(std::forward<Func>(task));
+
+            m_arena.enqueue([this, work = std::move(work)]() mutable {
+                m_taskGroup.run([work = std::move(work)]() mutable {
+                    work();
+                });
+            });
+        }
+
+        template<typename Func, typename Completion>
+        void SubmitWithCompletion(Func&& task, Completion&& onComplete)
+        {
+            if (m_shuttingDown.load(std::memory_order_acquire))
+                return;
+
+            auto work = std::function<void()>(std::forward<Func>(task));
+            auto completion = std::function<void()>(std::forward<Completion>(onComplete));
+
+            m_arena.enqueue([this, work = std::move(work), completion = std::move(completion)]() mutable {
+                m_taskGroup.run([this, work = std::move(work), completion = std::move(completion)]() mutable {
+                    work();
+
+                    m_threadingSystem.AddCompletedTask(completion);
+                });
+            });
+        }
+
+        void WaitAll();
+        size_t GetThreadCount() const noexcept { return m_threadCount; }
+
+    private:
+        ThreadingSystem& m_threadingSystem;
+        tbb::task_arena m_arena;
+        tbb::task_group m_taskGroup;
+        std::atomic<bool> m_shuttingDown{ false };
+        size_t m_threadCount{ 0 };
     };
 }
