35e64b3cdf5ccbd407eb1dab7dd714b955c3b9b5
KikoMira <mircevskihristijan30@gmail.com>
2025-11-05 16:52:25 +0100

    Create Node.h

---
+++ NodeGraph/Node.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include "../Engine/Define.h"
+#include "../Engine/StandardInclude.h"
+
+namespace BSE
+{
+    struct Component
+    {
+        virtual ~Component() = default;
+        virtual void InitComponent() {}
+        virtual void DeleteComponentData() {}
+        virtual void Update(double Tick) {}
+        virtual void Render(double Alpha) {}
+    };
+
+    class DLL_EXPORT Node
+    {
+    public:
+        explicit Node(std::string name)
+            : name(std::move(name)) {}
+        virtual ~Node() = default;
+
+        virtual bool InitNode()
+        {
+            for (auto& pair : components)
+            {
+                if (pair.second)
+                    pair.second->InitComponent();
+            }
+            return true;
+        }
+
+        virtual void DeleteNode()
+        {
+            for (auto& pair : components)
+            {
+                if (pair.second)
+                    pair.second->DeleteComponentData();
+            }
+        }
+
+        virtual void UpdateNode(double Tick)
+        {
+            for (auto& pair : components)
+            {
+                if (pair.second)
+                    pair.second->Update(Tick);
+            }
+
+            for (auto& pair : childrenByName)
+            {
+                auto& child = pair.second;
+                if (child)
+                    child->UpdateNode(Tick);
+            }
+        }
+
+        virtual void RenderNode(double Alpha)
+        {
+            for (auto& pair : components)
+            {
+                if (pair.second)
+                    pair.second->Render(Alpha);
+            }
+
+            for (auto& pair : childrenByName)
+            {
+                auto& child = pair.second;
+                if (child)
+                    child->RenderNode(Alpha);
+            }
+        }
+
+        std::string GetName() const { return name; }
+
+        bool HasChild(const std::string& childName) const
+        {
+            return childrenByName.find(childName) != childrenByName.end();
+        }
+
+        bool AddChild(std::shared_ptr<Node> node)
+        {
+            if (!node)
+                return false;
+
+            const auto& nodeName = node->GetName();
+            if (HasChild(nodeName))
+                return false;
+
+            childrenByName[nodeName] = std::move(node);
+            return true;
+        }
+
+        bool RemoveChild(const std::string& childName)
+        {
+            return childrenByName.erase(childName) > 0;
+        }
+
+        bool HasComponent(const std::string& compName) const
+        {
+            return components.find(compName) != components.end();
+        }
+
+        bool AddComponent(std::unique_ptr<Component> component, const std::string& compName)
+        {
+            if (!component)
+                return false;
+
+            if (HasComponent(compName))
+                return false;
+
+            components[compName] = std::move(component);
+            return true;
+        }
+
+        bool RemoveComponent(const std::string& compName)
+        {
+            return components.erase(compName) > 0;
+        }
+
+    private:
+        std::string name;
+        std::unordered_map<std::string, std::shared_ptr<Node>> childrenByName;
+        std::unordered_map<std::string, std::unique_ptr<Component>> components;
+    };
+}
