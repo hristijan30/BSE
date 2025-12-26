9c55c64e244e48900f963adc597b07188084eae8
KikoMira <mircevskihristijan30@gmail.com>
2025-12-26 10:11:06 +0100

    Update Engine.h

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -15,4 +15,30 @@ namespace BSE
         static uint64_t GetTotalRAM();
         static uint64_t GetAvailableRAM();
     };
+
+#ifdef _WIN32
+    #include <windows.h>
+    #include <shellapi.h>
+
+    inline bool CreateConsole()
+    {
+        if (AllocConsole())
+        {
+            FILE* fp;
+            freopen_s(&fp, "CONOUT$", "w", stdout);
+            freopen_s(&fp, "CONOUT$", "w", stderr);
+            freopen_s(&fp, "CONIN$", "r", stdin);
+            return true;
+        }
+        return false;
+    }
+
+    inline void DestroyConsole()
+    {
+        fclose(stdout);
+        fclose(stderr);
+        fclose(stdin);
+        FreeConsole();
+    }
+#endif
 }
