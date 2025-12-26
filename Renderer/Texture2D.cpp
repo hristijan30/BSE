b28c605389fb42d538a50f377abb5eb59f27ad7f
KikoMira <mircevskihristijan30@gmail.com>
2025-12-26 10:32:14 +0100

    build in AF x16

--- Renderer/Texture2D.cpp
+++ Renderer/Texture2D.cpp
@@ -52,6 +52,15 @@ namespace BSE
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+        float maxAniso = 16.0f;
+        glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &maxAniso);
+
+        glTexParameterf(
+            GL_TEXTURE_2D,
+            GL_TEXTURE_MAX_ANISOTROPY_EXT,
+            maxAniso
+        );
+
         glBindTexture(GL_TEXTURE_2D, 0);
 
         m_width = data.width;
